5c9eeeace7a147314212f656d8bd442fe4154c28
shh4und <alexandersznunes@gmail.com>
2024-06-02 23:36:19 -0300

    update config

--- config/config.go
+++ config/config.go
@@ -1,13 +1,28 @@
 package config
 
 import (
+	"errors"
+
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
 var (
-	db *gorm.DB
+	db     *gorm.DB
+	logger *Logger
 )
 
 func Init() error {
+
+	// get the env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		return errors.New("Error loading .env file")
+	}
 	return nil
+}
+
+func GetLogger(p string) *Logger {
+	logger = NewLogger(p)
+	return logger
 }
--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/shh4und/movie-tracker/config"
 	"github.com/shh4und/movie-tracker/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
-	// get the env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		panic("Error loading .env file")
-	}
+
+	logger = config.GetLogger("main")
 
 	// initialize configs
-	err = config.Init()
+	err := config.Init()
 	if err != nil {
-		panic("Error loading configs")
+		logger.Errorf("config init error: %v", err)
+		return
 	}
 
 	// initialize router
