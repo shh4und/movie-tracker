43b8e4c480003cce82c7ca1636de6c5c557bdab3
shh4und <alexandersznunes@gmail.com>
2024-06-19 16:59:23 -0300

    removed some unused fields

--- backend/schemas/users.go
+++ backend/schemas/users.go
@@ -4,46 +4,40 @@ import "time"
 
 // User represents the users table
 type User struct {
-	ID        uint      `gorm:"primaryKey"`
-	Username  string    `gorm:"size:50;unique;not null"`
-	Email     string    `gorm:"size:100;unique;not null"`
-	Password  string    `gorm:"size:100;not null"`
-	Minor     bool      `gorm:"not null"`
-	FirstName string    `gorm:"size:50"`
-	LastName  string    `gorm:"size:50"`
-	PhotoURL  string    `gorm:"size:255"`
-	Status    string    `gorm:"size:255"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"size:50;unique;not null"`
+	Email     string `gorm:"size:100;unique;not null"`
+	Password  string `gorm:"size:100;not null"`
+	Minor     bool   `gorm:"not null"`
+	FirstName string `gorm:"size:50"`
+	LastName  string `gorm:"size:50"`
+	PhotoURL  string `gorm:"size:255"`
+	Status    string `gorm:"size:255"`
 }
 
 // UserRating represents the user_ratings table
 type UserRating struct {
-	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null"`
-	MovieID   uint      `gorm:"not null"`
-	Rating    int       `gorm:"check:rating >= 1 AND rating <= 10"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
-	User      User      `gorm:"constraint:OnDelete:CASCADE;"`
+	ID      uint `gorm:"primaryKey"`
+	UserID  uint `gorm:"not null"`
+	MovieID uint `gorm:"not null"`
+	Rating  int  `gorm:"check:rating >= 1 AND rating <= 10"`
+	User    User `gorm:"constraint:OnDelete:CASCADE;"`
 }
 
 // UserFavorite represents the user_favorites table
 type UserFavorite struct {
-	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null"`
-	MovieID   uint      `gorm:"not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	User      User      `gorm:"constraint:OnDelete:CASCADE;"`
+	ID      uint `gorm:"primaryKey"`
+	UserID  uint `gorm:"not null"`
+	MovieID uint `gorm:"not null"`
+	User    User `gorm:"constraint:OnDelete:CASCADE;"`
 }
 
 // WatchLater represents the watch_later table
 type WatchLater struct {
-	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null"`
-	MovieID   uint      `gorm:"not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	User      User      `gorm:"constraint:OnDelete:CASCADE;"`
+	ID      uint `gorm:"primaryKey"`
+	UserID  uint `gorm:"not null"`
+	MovieID uint `gorm:"not null"`
+	User    User `gorm:"constraint:OnDelete:CASCADE;"`
 }
 
 // WatchedMovie represents the watched_movies table
@@ -52,17 +46,14 @@ type WatchedMovie struct {
 	UserID    uint `gorm:"not null"`
 	MovieID   uint `gorm:"not null"`
 	WatchedOn time.Time
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	User      User      `gorm:"constraint:OnDelete:CASCADE;"`
+	User      User `gorm:"constraint:OnDelete:CASCADE;"`
 }
 
 type Comment struct {
-	ID        uint      `gorm:"primaryKey"`
-	UserID    uint      `gorm:"not null"`
-	MovieID   uint      `gorm:"not null"`
-	Text      string    `gorm:"not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
-	User      User      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
-	Title     Title     `gorm:"foreignKey:MovieID;constraint:OnDelete:CASCADE"`
+	ID      uint   `gorm:"primaryKey"`
+	UserID  uint   `gorm:"not null"`
+	MovieID uint   `gorm:"not null"`
+	Text    string `gorm:"not null"`
+	User    User   `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	Title   Title  `gorm:"foreignKey:MovieID;constraint:OnDelete:CASCADE"`
 }
