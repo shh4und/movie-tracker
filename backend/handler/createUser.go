7f749865fc1e945cdb86f8bca69e642997ba7455
shh4und <alexandersznunes@gmail.com>
2024-06-27 20:54:25 -0300

    now createUser uses hashed password

--- backend/handler/createUser.go
+++ backend/handler/createUser.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shh4und/movie-tracker/handler/auth"
 	"github.com/shh4und/movie-tracker/schemas"
 )
 
@@ -17,16 +18,22 @@ func CreateUser(ctx *gin.Context) {
 		return
 	}
 
+	hashedPassword, err := auth.HashPassword(request.Password)
+	if err != nil {
+		logger.Errorf("error hashing password: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+	}
+
 	newUser := schemas.User{
 		Username: request.Username,
 		Email:    request.Email,
-		Password: request.Password,
+		Password: hashedPassword,
 		Minor:    request.Minor,
 	}
 
 	if err := db.Create(&newUser).Error; err != nil {
 		logger.Errorf("error creating user: %v", err.Error())
-		sendError(ctx, http.StatusInternalServerError, "error on database -> ")
+		sendError(ctx, http.StatusInternalServerError, err.Error())
 		return
 	}
 
