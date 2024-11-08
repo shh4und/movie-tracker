617faf7fa9632ee175241b24155a0c34c2ecd178
shh4und <alexandersznunes@gmail.com>
2024-11-08 19:36:29 -0300

    making the names more general and maping the Titles struct for the titles table in the db

--- backend/models/models.go
+++ backend/models/models.go
@@ -4,26 +4,28 @@ import "time"
 
 // Title represents the titles table in the database and the API response
 type Title struct {
-	ID         uint   `json:"id"`
-	Title      string `json:"Title"`
-	Year       string `json:"Year"`
-	Rated      string `json:"Rated"`
-	Released   string `json:"Released"`
-	Runtime    string `json:"Runtime"`
-	Genre      string `json:"Genre"`
-	Director   string `json:"Director"`
-	Writer     string `json:"Writer"`
-	Actors     string `json:"Actors"`
-	Plot       string `json:"Plot"`
-	Language   string `json:"Language"`
-	Country    string `json:"Country"`
-	Awards     string `json:"Awards"`
-	Poster     string `json:"Poster"`
-	IMDBRating string `json:"imdbRating"`
-	IMDBID     string `json:"imdbID"`
-	Type       string `json:"Type"`
-	Production string `json:"Production"`
-	Response   string `json:"Response"`
+	ID         uint      `json:"id"`
+	Title      string    `json:"Title"`
+	Year       string    `json:"Year"`
+	Rated      string    `json:"Rated"`
+	Released   string    `json:"Released"`
+	Runtime    string    `json:"Runtime"`
+	Genre      string    `json:"Genre"`
+	Director   string    `json:"Director"`
+	Writer     string    `json:"Writer"`
+	Actors     string    `json:"Actors"`
+	Plot       string    `json:"Plot"`
+	Language   string    `json:"Language"`
+	Country    string    `json:"Country"`
+	Awards     string    `json:"Awards"`
+	Poster     string    `json:"Poster"`
+	IMDBRating string    `json:"imdbRating"`
+	IMDBID     string    `json:"imdbID"`
+	Type       string    `json:"Type"`
+	Production string    `json:"Production"`
+	Response   string    `json:"Response"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
 
 // Search represents a search with results the API response
@@ -49,7 +51,7 @@ type User struct {
 type UserRating struct {
 	ID      uint `json:"id"`
 	UserID  uint `json:"user_id"`
-	MovieID uint `json:"movie_id"`
+	TitleID uint `json:"title_id"`
 	Rating  int  `json:"rating"`
 }
 
@@ -57,21 +59,21 @@ type UserRating struct {
 type UserFavorite struct {
 	ID      uint `json:"id"`
 	UserID  uint `json:"user_id"`
-	MovieID uint `json:"movie_id"`
+	TitleID uint `json:"title_id"`
 }
 
 // WatchLater represents the watch_later table
 type WatchLater struct {
 	ID      uint `json:"id"`
 	UserID  uint `json:"user_id"`
-	MovieID uint `json:"movie_id"`
+	TitleID uint `json:"title_id"`
 }
 
 // WatchedMovie represents the watched_movies table
 type WatchedMovie struct {
 	ID        uint      `json:"id"`
 	UserID    uint      `json:"user_id"`
-	MovieID   uint      `json:"movie_id"`
+	TitleID   uint      `json:"title_id"`
 	WatchedOn time.Time `json:"watched_on"`
 }
 
@@ -79,6 +81,6 @@ type WatchedMovie struct {
 type Comment struct {
 	ID      uint   `json:"id"`
 	UserID  uint   `json:"user_id"`
-	MovieID uint   `json:"movie_id"`
+	TitleID uint   `json:"title_id"`
 	Text    string `json:"text"`
 }
