43714d8edd9b8409115979804e66f845f2031352
shh4und <alexandersznunes@gmail.com>
2024-11-12 23:04:28 -0300

    some corrections

--- backend/handler/getTitle.go
+++ backend/handler/getTitle.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -37,25 +36,26 @@ func GetTitle(ctx *gin.Context) {
 
 	title, err = pgx.CollectOneRow(rows, pgx.RowToAddrOfStructByPos[models.Title])
 	if err != nil {
-		fmt.Println("\nERROR at CollectOneRow: \n" + err.Error())
 		title, err = services.FetchTitleFromOMDB(normalizedTitleName)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error at FetchTitleFromOMDB": err.Error()})
+			logger.Errorf("error at FetchTitleFromOMDB: %v", err.Error())
+			sendError(ctx, http.StatusInternalServerError, err.Error())
 			return
 		}
 		// Insert the title into the titles table
-		_, err = tx.Exec(ctx, `
+		err = tx.QueryRow(ctx, `
             INSERT INTO titles (title, year, rated, released, runtime, genre, director, writer, actors, plot, language, country, awards, poster, imdb_rating, imdb_id, type, production, response, created_at, updated_at)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        `, strings.ToLower(title.Title), title.Year, title.Rated, title.Released, title.Runtime, title.Genre, title.Director, title.Writer,
+            RETURNING id
+        `, title.Title, title.Year, title.Rated, title.Released, title.Runtime, title.Genre, title.Director, title.Writer,
 			title.Actors, title.Plot, title.Language, title.Country, title.Awards, title.Poster, title.IMDBRating, title.IMDBID,
-			title.Type, title.Production, title.Response)
+			title.Type, title.Production, title.Response).Scan(&title.ID)
 		if err != nil {
 			logger.Errorf("Failed to insert title: %v", err.Error())
 			sendError(ctx, http.StatusInternalServerError, err.Error())
 			return
 		}
-
+		logger.Infof("the title: %v have been cached", normalizedTitleName)
 	}
 
 	if err = tx.Commit(ctx); err != nil {
