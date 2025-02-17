bc055b4f9e2f1e676f7aef4807c5aba6a9604aca
shh4und <alexandersznunes@gmail.com>
2025-02-17 17:16:00 -0300

    removing gin dependencies and moved protected/public routes into other modules

--- backend/router/routes.go
+++ backend/router/routes.go
@@ -1,50 +1,19 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/shh4und/movie-tracker/auth"
-	"github.com/shh4und/movie-tracker/config"
+	"net/http"
+
 	"github.com/shh4und/movie-tracker/handler"
 	"github.com/shh4und/movie-tracker/services"
 )
 
-func initRoutes(router *gin.Engine) {
-
+func initRoutes(mux *http.ServeMux) {
 	handler.InitHandler()
 	services.InitServices()
-	secret := []byte(config.Envs.JwtToken)
 
 	// Public routes
-	public := router.Group("/api/v1")
-	{
-		public.POST("/register", handler.CreateUser) // create
-		public.POST("/login", handler.LoginUser)     // authenticate
-		// fetch a list of a title search
-	}
+	initPublicRoutes(mux)
 
 	// Protected routes
-	protected := router.Group("/api/v1")
-	protected.Use(auth.Authenticate(secret)) // using middleware to verify the authenticate session
-	{
-		// CRUD user routes
-		protected.GET("/users/profile", handler.GetUserProfileByUsername)
-		protected.PUT("/users/update", handler.UpdateUser)
-		protected.DELETE("/users/delete", handler.DeleteUser)
-
-		// User actions
-		protected.GET("/titles/search", handler.GetTitlesSearch)
-
-		protected.POST("/rate", handler.AddRating)
-		protected.DELETE("/rate", handler.RemoveRating)
-
-		protected.POST("/comment", handler.AddComment)
-
-		protected.POST("/favorite", handler.AddFavorite)
-		protected.DELETE("/favorite", handler.RemoveFavorite)
-
-		protected.POST("/watchlater", handler.AddWatchLater)
-
-		protected.POST("/watched", handler.AddWatched)
-	}
-
+	initProtectedRoutes(mux)
 }
